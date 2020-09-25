dfb15bbb8717fb93536dcc5387ccb5a0f608b217
Gabriel CHIA <gabriel_chia@NECSAPSIN.COM>
2020-09-25 17:13:51 +0800

    Initial project

---
+++ Mergesort/Mergesort.cpp
@@ -0,0 +1,14 @@
+// Mergesort.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+int numbercases[10] = { 300,2392,493,504,2792,659,112,20,30,9 };
+
+int main()
+{
+    return 0;
+}
+
