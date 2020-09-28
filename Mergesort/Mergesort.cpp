4cfc33fbe2a9a88792df4ae92b88e3c560d2c079
Gabriel CHIA <gabriel_chia@NECSAPSIN.COM>
2020-09-28 10:46:51 +0800

    Added memory optimisations

--- Mergesort/Mergesort.cpp
+++ Mergesort/Mergesort.cpp
@@ -8,11 +8,7 @@ using namespace std;
 int* numbercases = new int[10]{ 300, 2392, 493, 504, 2792, 659, 112, 20, 30, 9 };
 
 int* mergesort(int* arr, int len) {
-	//cout << "input: ";
-	//for (int i = 0; i < len; i++)
-		//cout << arr[i] << " ";
 
-	//cout << endl;
 	int halvedLen = len / 2;
 
 	if (len == 2) {
@@ -31,37 +27,33 @@ int* mergesort(int* arr, int len) {
 
 	int* firstH = new int[halvedLen];
 	int* secH = new int[len - halvedLen];
-	//cout <<endl<< "input A0: ";
-	for (int i = 0; i < halvedLen; i++) {
-		firstH[i] = arr[i];
-		//cout << firstH[i] << " ";
-	}
-	//cout <<endl<< "input A1: ";
-	for (int i = halvedLen; i < len; i++) {
-		secH[i - halvedLen] = arr[i];
-		//cout << secH[i - halvedLen] << " ";
-	}
 
+	for (int i = 0; i < halvedLen; i++) 
+		firstH[i] = arr[i];
+	
+	for (int i = halvedLen; i < len; i++) 
+		secH[i - halvedLen] = arr[i];
+	
 	firstH = mergesort(firstH, halvedLen);
 	secH = mergesort(secH, len - halvedLen);
 
 	int fHT = 0;
 	int sHT = 0;
 
-	for (int i = 0; i < len; i++) {
-
+	for (int i = 0; i < len; i++) 
 		if ((fHT < halvedLen && firstH[fHT] < secH[sHT]) || sHT >= len - halvedLen) {
 			arr[i] = firstH[fHT];
-			fHT++;			
+			fHT++;
 		}
 		else {
 			//cout << secH[sHT] << " ";
 			arr[i] = secH[sHT];
 			sHT++;
 		}
-	}
-	//delete[] firstH;
-	//delete[] secH;
+	
+
+	delete[] firstH;
+	delete[] secH;
 
 	return arr;
 }
